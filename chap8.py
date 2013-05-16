26516cfa8c21d1afdedb2f1af879a9db6ba772e2
joytafty <joytafty@gmail.com>
2013-05-15 21:01:28 -0400

    commit chap8 and chap9 exercises

--- chap8.py
+++ chap8.py
@@ -1,2 +1,19 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Tharathorn (Joy) Rimchala
+
+def rotate_char(c,nc):
+	return chr(ord(c)+nc)
+
+def rotate_word(s,ns):
+	ss = '';
+	for js in range (len(s)):
+		ss = ss + rotate_char(s[js],ns)
+	return ss
+
+# Test 
+def printmsg(s,n):
+	print 'rotate_char(' + s[0] + ',' + str(n) + ') = ' + rotate_char(s[0],n) 
+	print 'rotate_word(' + s + ',' + str(n) + ') = ' + rotate_word(s,n)
+
+printmsg('cheer',7)
+printmsg('melon',-10)
--- chap9.py
+++ chap9.py
@@ -1,2 +1,80 @@
 # This is where the answers to Chapter 9 questions for the BSS Dev RampUp go
-# Name:
+# Name: Tharathorn (Joy) Rimchala
+
+# Exercise 9.1
+def printlong(word):
+	plen = 20
+	if (len(word) > plen):
+		print word
+
+# Exercise 9.1 calls
+print "# Exercise 9.1 #"
+print "================"
+f1 = open('words.txt')
+for line in f1:
+	word = line.strip()
+	printlong(word)
+
+# Exercise 9.2
+def has_no_e(word):
+	if ("e" not in word):
+		print word
+		return 1
+	else: 
+		return 0
+
+# Exercise 9.2 calls
+print "# Exercise 9.2 #"
+print "================"
+f2 = open('words.txt')
+n_noe = 0
+nw = 0
+for line in f2:
+	nw = nw+1
+	word = line.strip()
+	temp = has_no_e(word)
+	n_noe = n_noe + temp
+f_noe = float(n_noe)/float(nw)
+print "Percentage of words with no 'e' :" + str(f_noe)
+	
+# Exercise 9.3
+def avoids(word, c):
+	return ("c" not in word)
+
+def avoids2(fin, c):
+	for line in fin:
+		word = line.strip()		
+		if not(set(c).issubset(set(word))):
+			print word
+# Exercise 9.3 calls
+print "# Exercise 9.3.1 #"
+print "=================="
+p1 = "Please provide a word input to test : "
+c1 = raw_input(p1)
+p2 = "Please provide a character or string to avoid: "
+c2 = raw_input(p2)
+b = avoids(c1,c2)
+print str(b)
+
+print "# Exercise 9.3.2 #"
+print "=================="
+f3 = open('words.txt')
+p = "Please provide word or string of forbidden letters :"
+c = raw_input(p)
+avoids2(f3,c)
+
+# Exercise 9.4
+def uses_only(word,c):
+	return (set(word).issubset(set(c)))
+
+# Exercise 9.4 calls
+print "# Exercise 9.4 #"
+print "=================="
+p4 = "Please provide word to test :"
+c4 = raw_input(p4)
+p5 = "Please provide word or string of allowed letters :"
+c5 = raw_input(p5)
+b5 = uses_only(c4,c5)
+print str(b5)
+
+
